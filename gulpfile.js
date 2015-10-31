d8800bce6e3c20f77bc236be46036ba5e8e98c3e
Noé Maillard <noemaillard@gmail.com>
2015-10-31 12:36:11 +0100

    Ajout de livereload pour visualiser les changements css sans rechager la page (je pense que c'est possible pour le html et js)

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,20 @@
 var gulp       = require('gulp');  
 var less       = require('gulp-less');  
 var watch      = require('gulp-watch');
+var livereload = require('gulp-livereload');
 
 /* Task to compile less */
 gulp.task('compile-less', function() {  
   gulp.src('./public/less/styles.less')
-    .pipe(less())
-    .pipe(gulp.dest('./public/css/'));
+  .pipe(less())
+  .pipe(gulp.dest('./public/css/'))
+  .pipe(livereload());
 });
 
 /* Task to watch less changes */
-gulp.task('watch-less', function() {  
-  gulp.watch('./public/less/styles.less' , ['compile-less']);
+gulp.task('watch-less', function() { 
+    livereload.listen(); 
+    gulp.watch('./public/less/styles.less' , ['compile-less']);
 });
 
 /* Task when running `gulp` from terminal */
