f245c270612d2ad4696aa3ad4021b5771628bdd3
Noé Maillard <noemaillard@gmail.com>
2015-10-31 15:21:09 +0100

    le contenu scrolle au dessus de l'image et sous le header

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,22 @@
 $(function() {
-    
+    var previousScroll = 0;
     $(window).on("scroll", function () {
-        if ($(this).scrollTop() > 0) {
-            $("#nav-header").css('height', $("#nav-header").height() - $(this).scrollTop()/100 + 'px');
+        var currentScroll = $(this).scrollTop();
+        if (currentScroll > 0 && $("#nav-header").height() > 50) {
+            $("#nav-header").css('height', $("#nav-header").height() - currentScroll/10 + 'px');
         }
-        else {
-        }
+        else if (currentScroll == 0) {
+            $("#nav-header").css('height', '100px');            
+        };
+        var offres = $("#offres");
+        var position = offres.position();
+        console.log(position.top);
+        if(currentScroll > 0 && position.top > 300) {
+            offres.css('top', -currentScroll);
+        } else if (currentScroll < previousScroll) {
+            offres.css('top', -currentScroll);
+        };;
+        previousScroll = currentScroll;
     });
 
 
