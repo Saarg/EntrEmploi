1a45acf387c674b36c9acfb7837cc2e312bf3d42
Noé Maillard <noemaillard@gmail.com>
2015-10-31 16:31:41 +0100

    avec le footer maintenant

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,16 @@
 $(function() {
     var previousScroll = 0;
     $(window).on("scroll", function () {
+
         var currentScroll = $(this).scrollTop();
+
         if (currentScroll > 0 && $("#nav-header").height() > 50) {
             $("#nav-header").css('height', $("#nav-header").height() - currentScroll/10 + 'px');
         }
         else if (currentScroll == 0) {
             $("#nav-header").css('height', '100px');            
         };
-        var offres = $("#offres");
+        var offres = $(".parallaxe");
         var position = offres.position();
         console.log(position.top);
         if(currentScroll > 0 && position.top > 300) {
