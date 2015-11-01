fde076e7773909398a7dee5572f925a944973b70
Noé Maillard <noemaillard@gmail.com>
2015-11-01 17:03:27 +0100

    fix de l'espace blanc à la fin de la page

--- public/js/main.js
+++ public/js/main.js
@@ -5,19 +5,16 @@ $(function() {
         var currentScroll = $(this).scrollTop();
 
         if (currentScroll > 0 && $("#nav-header").height() > 50) {
+            // ajustement de la taille du header
             $("#nav-header").css('height', $("#nav-header").height() - currentScroll/10 + 'px');
         }
         else if (currentScroll == 0) {
             $("#nav-header").css('height', '100px');            
         };
         var offres = $(".parallaxe");
-        var position = offres.position();
-        console.log(position.top);
-        if(currentScroll > 0 && position.top > 300) {
-            offres.css('top', -currentScroll);
-        } else if (currentScroll < previousScroll) {
-            offres.css('top', -currentScroll);
-        };;
+        if(currentScroll >0) {
+            offres.css('margin-top', -currentScroll); // effet de paxallaxe
+        };
         previousScroll = currentScroll;
     });
 
