ce229f1381ca0e56243450a841622c97c2288ee8
Noé Maillard <noemaillard@gmail.com>
2015-11-01 23:32:26 +0100

    Ajout du html au livereload

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,31 @@ var gulp       = require('gulp');
 var less       = require('gulp-less');  
 var watch      = require('gulp-watch');
 var livereload = require('gulp-livereload');
+var minify     = require('gulp-minify-css');
 
-/* Task to compile less */
+// LESS
 gulp.task('compile-less', function() {  
   gulp.src('./public/less/styles.less')
   .pipe(less())
+  .pipe(minify())
   .pipe(gulp.dest('./public/css/'))
   .pipe(livereload());
 });
 
+// HTML
+gulp.task('html', function() {
+    return gulp.src([
+        './public/**/*.html'
+    ])
+    .pipe(livereload());
+});
+
 /* Task to watch less changes */
-gulp.task('watch-less', function() { 
+gulp.task('watch', function() { 
     livereload.listen(); 
     gulp.watch('./public/less/styles.less' , ['compile-less']);
+    gulp.watch('./public/**/*.html', ['html']);
 });
 
 /* Task when running `gulp` from terminal */
-gulp.task('default', ['compile-less', 'watch-less']); 
+gulp.task('default', ['compile-less', 'html', 'watch']); 
