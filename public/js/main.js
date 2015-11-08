f65ac67cde3208ec92e00fb34d424fdec87fadb4
Noé Maillard <noemaillard@gmail.com>
2015-11-08 17:14:10 +0100

    enlevé la parallaxe

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,4 @@
 $(function() {
-    var previousScroll = 0;
-    $(window).scroll(function () {
-
-        var currentScroll = $(this).scrollTop();
-
-        if (currentScroll > 0 && $("#nav-header").height() > 50) {
-            // ajustement de la taille du header quand on scrolle vers le bas
-            $("#nav-header").css('height', $("#nav-header").height() - currentScroll/10.0 + 'px');
-            $('.header-spacer').css('transform', 'translateY(' + 100 - currentScroll/10.0 + 'px)');
-
-        }
-        else if (currentScroll == 0) {
-            // reset quand on arrive a 0
-            $("#nav-header").css('height', '100px');
-            $('.header-spacer').css('transform', 'translateY(' + 100 + 'px)');
-
-        }
-
-        var scrollHeight = $(document).height();
-        var scrollPosition = $(window).height() + $(window).scrollTop();
-        if ((scrollHeight - scrollPosition) / scrollHeight !== 0) { /* fix pour le bounce en bas de page */
-            var offres = $(".parallaxe");
-            if(currentScroll > 0) {
-                offres.css('margin-top', -currentScroll);
-            }
-        }
-        previousScroll = currentScroll;
-    });
 
     $('#carousel-temoins').carousel({
 
