a8c985e159966896029ebb9ab8a8a181b3943540
Noé Maillard <noemaillard@gmail.com>
2015-11-19 00:56:24 +0100

    petite image

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,6 +1,6 @@
 angular.module('MainCtrl', ['ui.bootstrap', 'ngAnimate']).controller('MainController', function($scope, MainArticle) {
     // Trucs pour le carousel
-    $scope.intervalImages = 1000;
+    $scope.intervalImages = 7000;
     $scope.interval = 5000;
     $scope.slides = [
     {
