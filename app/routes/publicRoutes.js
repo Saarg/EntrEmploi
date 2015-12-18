2cb4cfef21450db547943297280b32b132c56e4c
saarg <milsonneau.jean@free.fr>
2015-12-18 20:37:34 +0100

    ajout de route a l'API pour compter les elements

--- app/routes/publicRoutes.js
+++ app/routes/publicRoutes.js
@@ -4,6 +4,7 @@ var Entreprises = require('./../models/Partenaires');
 var MainArticles = require('./../models/MainArticles');
 
 module.exports = function(app) {
+    // Offres
     app.get('/api/offres', function(req, res) {
         Offres.find(function(err, offres) {
             if (err)
@@ -11,25 +12,56 @@ module.exports = function(app) {
             res.json(offres);
         });
     });
+    app.get('/api/offres/count', function(req, res) {
+        Offres.count(function(err, count) {
+            if (err)
+                res.send(err)
+            res.json(count);
+        });
+    });
+    // Profils
     app.get('/api/profils', function(req, res) {
         Profils.find(function(err, profils) {
             if (err)
                 res.json({ success: false, message: err });
-            res.json(profils); 
+            res.json(profils);
         });
     });
+    app.get('/api/profils/count', function(req, res) {
+        Profils.count(function(err, count) {
+            if (err)
+                res.send(err)
+            res.json(count);
+        });
+    });
+    // Partenaires
     app.get('/api/partenaires', function(req, res) {
         Entreprises.find(function(err, entreprises) {
             if (err)
                 res.json({ success: false, message: err });
-            res.json(entreprises); 
+            res.json(entreprises);
         });
     });
+    app.get('/api/partenaires/count', function(req, res) {
+        Entreprises.count(function(err, count) {
+            if (err)
+                res.send(err)
+            res.json(count);
+        });
+    });
+    // Articles
     app.get('/api/mainArticles', function(req, res) {
         MainArticles.find(function(err, mainArticles) {
             if (err)
                 res.json({ success: false, message: err });
-            res.json(mainArticles); 
+            res.json(mainArticles);
+        });
+    });
+    app.get('/api/mainArticles/count', function(req, res) {
+        MainArticles.count(function(err, count) {
+            if (err)
+                res.send(err)
+            res.json(count);
         });
     });
 }
