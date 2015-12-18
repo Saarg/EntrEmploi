b799124be7da9c8547c65d57d4e2a4291ebaec5b
Noé Maillard <noemaillard@gmail.com>
2015-12-18 22:17:24 +0100

    fonction getOffres -> OffresService

--- app/routes/Offres.js
+++ app/routes/Offres.js
@@ -3,49 +3,49 @@ var Offres = require('./../models/Offres');
 module.exports = function(app) {
     // POST
     app.post('/api/offres', function(req, res) {
-	var offre = new Offres();
-	offre.titre = req.body.titre;
-	offre.contenu = req.body.contenu;
-	offre.entreprise = req.body.entreprise;
-	offre.siteEntreprise = req.body.siteEntreprise;
-	offre.numContact = req.body.numContact;
-	offre.mailContact = req.body.mailContact;
-	offre.date = Date();
-	offre.dateFin = req.body.dateFin;
-	offre._createur = req.body._createur;
+        var offre = new Offres();
+        offre.titre = req.body.titre;
+        offre.contenu = req.body.contenu;
+        offre.entreprise = req.body.entreprise;
+        offre.siteEntreprise = req.body.siteEntreprise;
+        offre.numContact = req.body.numContact;
+        offre.mailContact = req.body.mailContact;
+        offre.date = Date();
+        offre.dateFin = req.body.dateFin;
+        offre._createur = req.body._createur;
 
-	offre.save(function(err) {
+        offre.save(function(err) {
             if (err)
-                res.json({ success: false, message: err });
+            res.json({ success: false, message: err });
             res.json({ success: true });
         });
     });
     // PUT
     app.put('/api/offres/:offre_id', function(req, res) {
-	Offres.findById(req.params.offre_id, function(err, offre) {
+        Offres.findById(req.params.offre_id, function(err, offre) {
             if (err)
-                res.send(err);
+            res.send(err);
             offre.titre = req.body.titre;
-	    offre.contenu = req.body.contenu;
-	    offre.entreprise = req.body.entreprise;
-	    offre.siteEntreprise = req.body.siteEntreprise;
-	    offre.numContact = req.body.numContact;
-	    offre.mailContact = req.body.mailContact;
-	    offre.dateFin = req.body.dateFin;
+            offre.contenu = req.body.contenu;
+            offre.entreprise = req.body.entreprise;
+            offre.siteEntreprise = req.body.siteEntreprise;
+            offre.numContact = req.body.numContact;
+            offre.mailContact = req.body.mailContact;
+            offre.dateFin = req.body.dateFin;
 
             offre.save(function(err) {
                 if (err)
-                    res.json({ success: false, message: err });
-		res.json({ success: true });
+                res.json({ success: false, message: err });
+                res.json({ success: true });
             });
         });
     });
     // DELETE
     app.delete('/api/offres/:offre_id', function(req, res) {
-	Offres.remove({_id: req.params.offre_id}, function(err, offre) {
+        Offres.remove({_id: req.params.offre_id}, function(err, offre) {
             if (err)
-                res.json({ success: false, message: err });
+            res.json({ success: false, message: err });
             res.json({ success: true });
-        });	
+        });
     });
 }
--- public/js/services/OffresService.js
+++ public/js/services/OffresService.js
@@ -1,8 +1,10 @@
 angular.module('OffresService', []).factory('Offre', ['$http', function($http) {
     return {
         // appel pour recup les offres
-        getOffres : function() {
+        getOffres : (function() {
             return $http.get('/api/offres');
-        }
+        })().then(function (res) {
+            return res.data;
+        })
     }
 }]);
