00090c1e9a674adce82f6a393743fdb6ae425ea1
Noé Maillard <noemaillard@gmail.com>
2015-12-20 01:22:26 +0100

    update package.json

--- app/routes/Auth.js
+++ app/routes/Auth.js
@@ -1,6 +1,6 @@
 // modules =================================================
 var jwt            = require('jsonwebtoken');
-var User           = require('../../app/models/Staff');
+var Users           = require('../../app/models/Staff');
 
 module.exports = function(app) {
     app.post('/token/verify', function(req, res) {
@@ -8,37 +8,37 @@ module.exports = function(app) {
         // decode token
         if (token) {
             jwt.verify(token, app.get('superSecret'), function(err, decoded) {
-            if (err) {
-                res.json({ success: false, message: 'Failed to authenticate token.' });
-            } else {
-                res.json({ success: true, decoded: decoded });
-            }
+                if (err) {
+                    res.json({ success: false, message: 'Failed to authenticate token.' });
+                } else {
+                    res.json({ success: true, decoded: decoded });
+                }
             });
 
         } else {
             res.status(403).send({
-            success: false,
-            message: 'No token provided.'
+                success: false,
+                message: 'No token provided.'
             });
         }
     });
 
     // process the login form
     app.post('/login', function(req, res){
-        nom         = req.body.nom;
-        prenom      = req.body.prenom;
-        password    = req.body.passwd;
+        var nom         = req.body.nom;
+        var prenom      = req.body.prenom;
+        var password    = req.body.passwd;
 
-        User.findOne({ 'nom' :  nom, 'prenom' :  prenom }, function(err, user) {
+        Users.findOne({ 'nom' :  nom, 'prenom' :  prenom }, function(err, user) {
             if (err)
                 res.json({ success: false, message: err });
             else if (!user)
                 res.json({ success: false, message: 'No user found' });
             else if (!user.validPassword(password))
                 res.json({ success: false, message: 'Oops Wrong password' });
-            else{
+            else {
                 var token = jwt.sign(user._id, app.get('superSecret'), {
-                            expiresIn: 86400 // expires in 24 hours
+                    expiresIn: 86400 // expires in 24 hours
                 });
                 res.json({ success: true, message:'bonjour', user: user, token: token });
             }
