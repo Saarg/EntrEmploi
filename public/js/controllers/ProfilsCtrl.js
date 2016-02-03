4d7ae5e2f72b4f6d159ffaa464005d951ea49501
Jean <milsonneau.jean@free.fr>
2016-02-03 21:58:42 +0100

    premier commit juste pour empecher l'acces sans login

--- public/js/controllers/ProfilsCtrl.js
+++ public/js/controllers/ProfilsCtrl.js
@@ -1,15 +1,27 @@
 angular.module('EntrEmploi').controller('ProfilsController', ProfilsController);
 
-ProfilsController.$inject = ['$scope', '$window', 'ProfilsService'];
+ProfilsController.$inject = ['$scope', '$window', 'ProfilsService', 'ngDialog'];
 
-function ProfilsController($scope, $window, ProfilsService) {
+function ProfilsController($scope, $window, ProfilsService, ngDialog) {
 
     ProfilsService.getProfils().then(function (res) {
         $scope.profils = res.data;
     });
 
+    $scope.loginEntreprise = function() {
+        console.log("TODO implementer passwordless");
+    }
+
     $scope.openCV = function(id) {
-        // TODO faire l'auth et check si le cv est upload
-        $window.open("CV/"+id, '_blank');
+        if($window.sessionStorage.token) {
+            $window.open("CV/"+id, '_blank');
+        } else {
+            ngDialog.open({
+                template : '../templates/loginEntreprise.html',
+                className: 'ngdialog-theme-default',
+                disableAnimation : true,
+                scope: $scope
+            });
+        }
     }
 }
