07811cdb27b1d9658ef976a3b3865b80cfc744e9
Jean <milsonneau.jean@free.fr>
2016-02-03 22:09:36 +0100

    oups

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var mongoose       	= require('mongoose');
 var bodyParser     	= require('body-parser');
 var methodOverride 	= require('method-override');
 
+var passwordless    = require('passwordless');
+var MongoStore      = require('passwordless-mongostore');
+
 var morgan      	= require('morgan');
 
 // configuration ===========================================
@@ -15,6 +18,30 @@ var db = require('./config/db');
 mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
 
 var port = process.env.PORT || 8080; // set our port
+
+// init passwordless
+passwordless.init(new MongoStore(db.url));
+// passwordless delivery token
+/*
+passwordless.addDelivery(
+    function(tokenToSend, uidToSend, recipient, callback) {
+        var host = 'localhost:3000';
+        smtpServer.send({
+            text:    'Hello!\nAccess your account here: http://'
+            + host + '?token=' + tokenToSend + '&uid='
+            + encodeURIComponent(uidToSend),
+            from:    yourEmail,
+            to:      recipient,
+            subject: 'Token for ' + host
+        }, function(err, message) {
+            if(err) {
+                console.log(err);
+            }
+            callback(err);
+        });
+});
+*/
+
 
 // get all data/stuff of the body (POST) parameters
 app.use(bodyParser.json()); // parse application/json
